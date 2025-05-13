a1e9002e516889dc90d627eef3ece4ad5fcc398c
Persii <tomiipersii@gmail.com>
2025-05-13 20:44:20 -0300

    ejercicio 1.3 better

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -36,15 +36,15 @@ const Header = (props) => {
 const Content = (props) => {
   return (
   <>
-        <Part part = {props.part1.name} exercises={props.part1.exercises}/>
-        <Part part= {props.part2.name} exercises={props.part2.exercises}/>
-        <Part part = {props.part3.name} exercises={props.part3.exercises}/>
+        <Part part = {props.part1} />
+        <Part part = {props.part2} />
+        <Part part = {props.part3} />
   </>
   )
 }
 
 const Part = (props) => {
-  return (<p> {props.part} : {props.exercises}</p> )
+  return (<p> {props.part.name} : {props.part.exercises}</p> )
 }
 
 const Total = (props) => {

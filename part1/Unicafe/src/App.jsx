3f431563b8c3bc82b7c5b32a10358a857d8a1ee1
Persii <tomiipersii@gmail.com>
2025-05-27 19:25:47 -0300

    Ejercicio 1.6 Done

---
+++ part1/Unicafe/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+const Goodcl = () => {
+  setGood(good +1)
+}
+
+const Neutralcl = () => {
+  setNeutral(neutral +1)
+}
+
+const Badcl = () => {
+  setBad(bad +1)
+}
+
+
+  return (
+    <div>
+      <h1>Danos una retroalimentacion porfi</h1>
+      <button onClick={Goodcl}> Bien </button>
+      <button onClick={Neutralcl}> Meh </button>
+      <button onClick={Badcl}> Malardo </button>
+      <h1>Estadisticas:</h1>
+      <p>Buenisimo: {good}</p>
+      <p>Meh: {neutral}</p>
+      <p>Malardo: {bad}</p>
+    </div>
+  )
+}
+
+export default App

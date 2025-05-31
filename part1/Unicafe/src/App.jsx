92c56fecc3cceb618c937a49044dd5b758596a86
Persii <tomiipersii@gmail.com>
2025-05-31 18:27:37 -0300

    Ejercicio 1.12 Done

--- part1/Unicafe/src/App.jsx
+++ part1/Unicafe/src/App.jsx
@@ -1,20 +1,29 @@
 import { useState } from 'react'
-import Statics from './Components/Statics'
 
 const App = () => {
-   const [good, setGood] = useState(0)
-   const [neutral, setNeutral] = useState(0)
-   const [bad, setBad] = useState(0)
+const anecdotes = [
+  'Wachín, cuchame... si te duele, hacelo más seguido.',
+  'Meter más gente en un proyecto atrasado es como echarle nafta al fuego. Spoiler: no ayuda.',
+  'El 90% del código se hace en el 10% del tiempo. El otro 10%... te arruina la vida durante meses.',
+  'Cualquiera puede escribir código que entienda la compu. Los buenos hacen que hasta tu abuela lo lea sin llorar.',
+  'Optimizar antes de tiempo es como ponerle nitro al auto... cuando todavía estás armando las ruedas.',
+  'Debuggear es el doble de difícil que codear. Si lo escribiste muy vivo, suerte pa\' entender qué hiciste.',
+  'Programar sin usar console.log es como ser médico y decir: “nah, los rayos X son de cheto”.',
+  'La única forma de ir rápido es... no mandarte cagadas, maestro.'
+]
 
-   const Button = ({click,text}) => (<button onClick={click}>{text}</button>)
+  const [selected, setSelected] = useState(0)
+
+  const Clickardo = () => {
+    const Nrandom = Math.floor(Math.random() * anecdotes.length)
+    setSelected(Nrandom)
+  }
 
   return (
     <div>
-      <h1>Danos una retroalimentacion porfi</h1>
-      <Button click={() => setGood(good+1)} text="Bien" />
-      <Button click={() => setNeutral(neutral+1)} text="Meh" />
-      <Button click={() => setBad(bad+1)} text="Malardo" />  
-      <Statics good= {good} neutral ={neutral} bad= {bad} />
+      <h1>Anecdotin del día</h1>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={Clickardo}>Mostrar otra anécdota</button>
     </div>
   )
 }
